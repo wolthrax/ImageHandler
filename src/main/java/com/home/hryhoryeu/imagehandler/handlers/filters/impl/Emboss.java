cae0efcc22c863d458dcbd8cd9177678cdbee94c
Maksim Hryhoryeu <MHryhoryeu@iba.by>
2018-10-19 17:43:11 +0300

    Start embross filter

--- src/main/java/com/home/hryhoryeu/imagehandler/handlers/filters/impl/Emboss.java
+++ src/main/java/com/home/hryhoryeu/imagehandler/handlers/filters/impl/Emboss.java
@@ -7,6 +7,8 @@ import com.home.hryhoryeu.imagehandler.handlers.filters.IFilter;
 import com.home.hryhoryeu.imagehandler.handlers.filters.masks.Mask;
 import com.home.hryhoryeu.imagehandler.handlers.filters.masks.presettings.EmbossPreset;
 import com.home.hryhoryeu.imagehandler.handlers.filters.masks.types.Mask3X3;
+import com.home.hryhoryeu.imagehandler.handlers.filters.masks.types.Mask5X5;
+import com.home.hryhoryeu.imagehandler.handlers.filters.masks.types.Mask7X7;
 import com.home.hryhoryeu.imagehandler.managers.IImageManager;
 import com.home.hryhoryeu.imagehandler.managers.impl.ImageManagerImpl;
 import javafx.scene.image.PixelWriter;
@@ -40,21 +42,46 @@ public class Emboss implements IFilter {
         for (int y = ConfigParams.Position.START+1; y < height-1; y++) {
             for (int x = ConfigParams.Position.START+1; x < width-1; x++) {
 
-                int R = imagePixelMap[x][y].getR();
-                int G = imagePixelMap[x][y].getG();
-                int B = imagePixelMap[x][y].getB();
-
                 double N_R = 0;
                 double N_G = 0;
                 double N_B = 0;
 
+                for (int i = -1; i <= 1; i++) {
+                    for (int j = -1; j <= 1; j++) {
+                        if (i != 0 && j != 0) {
+                            N_R += mask.getMask()[i + 1][j + 1] * imagePixelMap[x + i][y + j].getR();
+                            N_G += mask.getMask()[i + 1][j + 1] * imagePixelMap[x + i][y + j].getG();
+                            N_B += mask.getMask()[i + 1][j + 1] * imagePixelMap[x + i][y + j].getB();
+                        }
+                    }
+                }
+
+                N_R = N_R / 255d;
+                N_G = N_G / 255d;
+                N_B = N_B / 255d;
+
+//                if (N_R < ConfigParams.Value.MIN)
+//                    N_R = ConfigParams.Value.MIN;
+//                if (N_R > ConfigParams.Value.MAX)
+//                    N_R = ConfigParams.Value.MAX;
+//
+//                if (N_G < ConfigParams.Value.MIN)
+//                    N_G = ConfigParams.Value.MIN;
+//                if (N_G > ConfigParams.Value.MAX)
+//                    N_G = ConfigParams.Value.MAX;
+//
+//                if (N_B < ConfigParams.Value.MIN)
+//                    N_B = ConfigParams.Value.MIN;
+//                if (N_B > ConfigParams.Value.MAX)
+//                    N_B = ConfigParams.Value.MAX;
 
 
+                Color color = new Color(N_R, N_G, N_B, 1);
 
-                Color color = new Color(N_R/255, N_G/255, N_B/255, 1);
                 pixelWriter.setColor(x, y, color);
             }
         }
-
+        imageManager.setChangedImage(writableImage);
+        imageManager.applyChangedImage(writableImage);
     }
 }
